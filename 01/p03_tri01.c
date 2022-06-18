1470fe042fdcf5b242caa1f1ccd0eca85f722702
anxhuang <>
2022-06-18 15:53:57 +0800

    update p03

--- 01/p03_tri01.c
+++ 01/p03_tri01.c
@@ -4,7 +4,7 @@ int main() {
     int top = 10;
     int base = 15;
     int height = 7;
-    double area = ((top + base) * 7) / 2;
-    printf("%f", area);
+    double area = (top + base) * height / 2.0;
+    printf("%.1f", area);
 	return 0;
 }
